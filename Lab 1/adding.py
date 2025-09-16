bc4ece84298e043f7ce906349247ed759f296429
vedant sinha <vedantsinha3@gmail.com>
2025-09-16 11:19:05 -0700

    Fixed issue where built in sum was conflicting with var name

--- Lab 1/adding.py
+++ Lab 1/adding.py
@@ -1,19 +1,19 @@
-nums = input("Enter a list of numbers separated by spaces: ")
-nums = nums.split()
+nums_str = input("Enter a list of numbers separated by spaces: ")
+nums = nums_str.split()
+
 
 if len(nums) < 2: 
     print("Error: Please enter at least 2 numbers")
     exit()
 
-sum = 0 
-for i in range(len(nums)):
+
+total = 0 
+for num_str in nums:
     try: 
-        value = float(nums[i])
+        value = float(num_str)
+        total += value
     except ValueError:
-        print("Error: Please enter a valid number")
+        print(f"Error: '{num_str}' is not a valid number.")
         exit()
-    sum +=nums[i]
 
-
-print("The sum of the numbers is: ", sum)
-
+print(f"The sum of the numbers is: {total}")
